a56093469097b9fa82683f50242e9ad9f98d3d47
Johnny Ceelen <jceelen@eudonet.com>
2026-03-07 19:59:10 +0100

    Add github workflow

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue(), vueDevTools(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? '/mijn-vue-site/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/wp-tailwind/' : '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
